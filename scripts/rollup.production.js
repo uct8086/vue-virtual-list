fb787a05c9d2da036d8b518f113573877aabfdf0
uct8086 <belieftang@163.com>
2022-03-31 11:06:11 +0800

    fix: modify package logic

--- scripts/rollup.production.js
+++ scripts/rollup.production.js
@@ -1,11 +1,17 @@
 import babel from 'rollup-plugin-babel'
 import bannerString from './rollup.banner'
-
+import { nodeResolve } from '@rollup/plugin-node-resolve';
+// format选项
+// esm es6 模式
+// iife(Immediately-invoked function expression ) 立即调用模式
+// AMD
+// CommonJS
+// UMD
 export default {
     external: ['vue'],
     input: './src/index.js',
     output: {
-        format: 'umd',
+        format: 'es',
         file: './dist/index.js',
         name: 'VirtualList',
         sourcemap: false,
@@ -16,5 +22,6 @@ export default {
     },
     plugins: [
         babel(),
+        nodeResolve({ resolveOnly: ['@juggle/resize-observer'] }), // 这里指定包来解析，因为它是组件依赖包，Vue可以不解析，因为调用方会安装Vue
     ],
 }
